34919ee911236ffa0002c766cb2cfa0090134f93
Bhanu-Yeligeti <bhanuprasadyeligeti954@gmail.com>
2025-02-19 12:56:42 +0530

    commited

---
+++ lib/bhanu.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Bhanu extends StatefulWidget {
+  const Bhanu({super.key});
+
+  @override
+  State<Bhanu> createState() => _BhanuState();
+}
+
+class _BhanuState extends State<Bhanu> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text("hello"),
+
+        ],
+      ),
+    );
+  }
+}
